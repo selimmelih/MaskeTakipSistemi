12db09c7a9e9e1a08f1f263cf199f8a9ef8b9db0
Selim Melih <selimturhan37@gmail.com>
2024-03-04 21:09:45 +0300

    finish

---
+++ Workaround/Migrations/20240304180802_kendiPc.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Workaround.Migrations
+{
+    /// <inheritdoc />
+    public partial class kendiPc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
